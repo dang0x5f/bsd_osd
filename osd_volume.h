082908a6344b2490ac7337690f09ec097f858609
dang0x5f <d4n9xcv@gmail.com>
2025-07-10 23:49:53 -0400

    getvolume() added.

--- osd_volume.h
+++ osd_volume.h
@@ -19,14 +19,18 @@
 
 
 void increase_volume(void);
+float getvolume(void);
+void getspec1(float);
 
 #endif // OSD_VOLUME_H
 
 #ifdef OSD_VOLUME_IMPLEMENTATION
 
+#define SZ 20
+
 char *font_pattern = "Deja Vu Sans Mono:pixelsize=20";
 char *str2 = "|||||||||-----------";
-char *str = "VOLUME            45";
+char *def_line1 = "VOLUME              ";
 
 XftFont *font_setup(Display *display, int screen_num)
 {
@@ -70,13 +74,9 @@ void increase_volume(void)
     scrn_width  = XDisplayWidth(display,screen_num);
     scrn_height = XDisplayHeight(display,screen_num);
     valuemask   = CWOverrideRedirect|CWBackPixel|CWEventMask;
+    XftColorAllocName(display,visual,colormap,"#00FF00",&color);
 
-    XftTextExtentsUtf8(display,xftfont,str,strlen(str),&xgi);
-    // int win_width = xftfont->max_advance_width * strlen(str);
-    // printf("%d , %lu\n", xftfont->max_advance_width, strlen(str));
-    // printf("%dx%d, %d\n", win_width, win_height, xftfont->ascent);
-
-    int win_width = xgi.xOff;
+    int win_width  = xftfont->max_advance_width*SZ;
     int win_height = xftfont->height*2;
     int x = (scrn_width*0.5)-(win_width*0.5);
     int y = (scrn_height)*0.75;
@@ -86,8 +86,6 @@ void increase_volume(void)
 
     draw = XftDrawCreate(display,window,visual,colormap);
 
-    XftColorAllocName(display,visual,colormap,"#00FF00",&color);
-
     XMapWindow(display,window);
     XSync(display,false);
 
@@ -96,14 +94,45 @@ void increase_volume(void)
         XNextEvent(display,&event);
         switch(event.type){
             case Expose:
-                XftDrawStringUtf8(draw,&color,xftfont,0,xftfont->ascent,
-                                  (FcChar8*)str,strlen(str));
-                XftDrawStringUtf8(draw,&color,xftfont,0,xftfont->ascent*2,
-                                  (FcChar8*)str2,strlen(str2));
+                int lineno = 1;
+                float volume = getvolume();
+                // printf("%0.2f\n",volume);
+                XftGlyphFontSpec *spec1 = getspec1(volume);
                 break;
         }
     }
 
+}
+
+void getspec1(float)
+{
+
+}
+
+float getvolume(void)
+{
+    struct mixer *m;
+    mix_volume_t vol;
+    char *mix_name, *dev_name;
+
+    mix_name = NULL;
+    if((m=mixer_open(mix_name))==NULL)
+        err(1,"mixer_open: %s", mix_name);
+
+    dev_name = "vol";
+    if((m->dev=mixer_get_dev_byname(m,dev_name))<0)
+        err(1,"unknown device: %s", dev_name);
+
+    // printf("left: %0.2f right: %0.2f\n", m->dev->vol.left,m->dev->vol.right); 
+
+    // vol.left = m->dev->vol.left + 0.01;
+    // vol.right = m->dev->vol.right + 0.01;
+    // mixer_set_vol(m,vol);
+    // if(mixer_set_vol(m,vol) < 0)
+    //     warn("cannot change volume");
+    
+    // printf("left: %0.2f right: %0.2f\n", m->dev->vol.left,m->dev->vol.right); 
+    return(m->dev->vol.right);
 }
 
 void increase_volume1(void)
