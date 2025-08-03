4d118082d931bf79911b4556d20b8dc256619b9d
dang0x5f <d4n9xcv@gmail.com>
2025-08-02 21:08:11 -0400

    fixed: redesigned much of program.

--- osd_volume.h
+++ osd_volume.h
@@ -24,67 +24,59 @@ void osd_volume(char);
 
 #ifdef OSD_VOLUME_IMPLEMENTATION
 
+#define BLACK1 0x000000
+#define WHITE1 0xfffdd0
+#define GREEN "#00FF00"
 #define OSD_VOLUME_LOCK "/tmp/osd_volume.lck"
-#define PX_BETWEEN 1  // 1px between top and bottom
+#define PX_BETWEEN 1  
 #define SZ 20
 
+typedef struct {
+    uint32_t depth;
+    uint32_t width;
+    uint32_t height;
+    uint32_t attrmask;
+    int32_t xpos;
+    int32_t ypos;
+    Display* display;
+    Visual *visual;
+    XftFont *font;
+    XftDraw *dc;
+    Window win;
+    Window parent;
+    Colormap cmap;
+    XSetWindowAttributes xswa;
+    uint8_t screen;
+    uint8_t margin; 
+    XftColor fgcolor;
+    XftGlyphFontSpec *glyph; 
+    uint32_t line1_h;
+    uint32_t line2_h;
+} XWindow;
+
+void init_window(XWindow*);
 int is_muted(void);
-float getvolume(void);
 void sig_handler(int);
+float getvolume(void);
 void change_volume(char);
 int create_volume_lock(void);
+XftGlyphFontSpec *create_glyph(XWindow*,uint32_t);
+void draw(XWindow*);
+void draw_vol(XWindow*);
+void draw_glyph(XWindow*);
+void draw_label(XWindow*,char*);
 
 char *numeric_str;
 char *volume_str = "VOLUME";
 char *muted_str = "MUTED";
-
-void redraw(Display*,XftDraw*,Window*, uint32_t,XftColor*,XftFont*,XftGlyphFontSpec*,size_t);
-void draw_vol(Display *, XftDraw *,Window*, uint32_t,  XftColor *, XftFont *, size_t );
-void draw_spec(Display *, XftDraw *,Window*,  XftColor *, XftFont *, XftGlyphFontSpec *, size_t );
-XftGlyphFontSpec *create_spec(Display*,XftFont*,uint32_t,uint32_t,size_t);
-XftGlyphFontSpec *spec;
+XftGlyphFontSpec *glyph;
 
 float volume;
 bool timeup=false;
-int padding = 5;
 double duration = 5.0;
 time_t start_time, curr_time;
 
-char *font_pattern = "Deja Vu Sans Mono:pixelsize=20";
-
-void redraw(Display *display, XftDraw *draw, Window *window, uint32_t win_width, XftColor *color, XftFont *xftfont, XftGlyphFontSpec *spec, size_t sz)
-{
-    volume = getvolume();
-    draw_vol(display, draw, window, win_width, color, xftfont, SZ);
-    draw_spec(display, draw, window, color, xftfont, spec, SZ);
-}
-
-void draw_vol(Display *display, XftDraw *draw, Window *window, uint32_t win_width,  XftColor *color, XftFont *font, size_t sz)
-{
-    XClearArea(display,*window,win_width/2,0,0,font->ascent+padding,false);
-    if(is_muted()){
-        XftDrawStringUtf8(draw,color,font,(font->max_advance_width*15)+padding,font->ascent,(FcChar8*)muted_str,5);  
-    }else{
-        uint32_t vol = (uint32_t)(getvolume()*100);
-        char volbuffer[3] = {' ',' ',' '};
-        for(int i=2; i>=0; --i){
-            volbuffer[i] = (vol%10)+'0';
-            if(vol<10) break;
-            vol /= 10;
-        }
-        
-        XftDrawStringUtf8(draw,color,font,(font->max_advance_width*17)+padding,font->ascent,(FcChar8*)volbuffer,3);  
-        
-    }
-}
-
-void draw_spec(Display *display, XftDraw *draw, Window *window, XftColor *color, XftFont *font, XftGlyphFontSpec *spec, size_t sz)
-{
-    XClearArea(display,*window,0,font->ascent+font->descent+(padding),0,0,false);
-    uint8_t nblock = (volume*100)/5;
-
-    XftDrawGlyphFontSpec(draw,color,spec,nblock);
-}
+const char *font_name2 = "Deja Vu Sans Mono:pixelsize=20";
 
 void osd_volume(char operation)
 {
@@ -96,76 +88,27 @@ void osd_volume(char operation)
 
     signal(SIGUSR1,sig_handler);
 
-    Display *display;
-    int screen_num;
-    Colormap colormap;
-    int depth;
-    int scrn_width, scrn_height;
-    int valuemask;
-    Visual *visual;
-    Window root;
-    Window window;
-    XftColor color;
-    XftFont *xftfont;
-    XftDraw *draw;
-    XSetWindowAttributes attributes = { 
-        .override_redirect=true,
-        .background_pixel=0x000000,
-        .border_pixel=0xfffdd0,
-        .event_mask=ExposureMask|
-            VisibilityChangeMask|
-          SubstructureNotifyMask,
-    };
+    XWindow xwin = {0};
+    init_window(&xwin);
 
-    display     = XOpenDisplay(NULL);
-    root        = DefaultRootWindow(display);
-    screen_num  = DefaultScreen(display);
-    colormap    = DefaultColormap(display,screen_num);
-    xftfont     = font_setup(display,screen_num,font_pattern);
-    depth       = DefaultDepth(display,screen_num);
-    visual      = DefaultVisual(display,screen_num);
-    scrn_width  = XDisplayWidth(display,screen_num);
-    scrn_height = XDisplayHeight(display,screen_num);
-    valuemask   = CWOverrideRedirect|CWBackPixel|CWEventMask|CWBorderPixel;
-    XftColorAllocName(display,visual,colormap,"#00FF00",&color);
-
-    uint32_t nlines = 2;
-    uint32_t line_height = (xftfont->ascent+xftfont->descent);
-
-    int win_width  = (xftfont->max_advance_width*SZ) + (padding*2);
-    uint32_t win_height = (line_height*nlines) + (padding*2) +PX_BETWEEN;
-    int x = (scrn_width*0.5)-(win_width*0.5);
-    int y = (scrn_height)*0.75;
-    
-    window = XCreateWindow(display, root, x,y, win_width,win_height, 2, depth, 
-                           CopyFromParent, visual, valuemask, &attributes);
-
-    draw = XftDrawCreate(display,window,visual,colormap);
-
-    XMapWindow(display,window);
-    XSync(display,false);
-
-    XftDrawStringUtf8(draw,&color,xftfont, padding, xftfont->ascent, (FcChar8*)volume_str, 6);  
-
-    uint32_t height2 = line_height + xftfont->ascent + padding;
-    XftGlyphFontSpec *spec = create_spec(display,xftfont,0x2588,height2,SZ);
+    draw_label(&xwin, "VOLUME");
 
     time(&start_time);
     XEvent event;
     int mute_status = is_muted();
     double difference;
     while(!timeup){
-        XNextEvent(display,&event);
+        XNextEvent(xwin.display,&event);
         switch(event.type){
             case Expose:
-                redraw(display, draw, &window, win_width, &color, xftfont, spec, SZ);
+                draw(&xwin);
                 break;
             case VisibilityNotify:
-                XRaiseWindow(display,window);
+                XRaiseWindow(xwin.display,xwin.win);
                 break;
         }
         
-        usleep(50000);
+        usleep(33333);
         time(&curr_time);
         difference = difftime(curr_time,start_time);
 
@@ -175,14 +118,148 @@ void osd_volume(char operation)
             temp.type=Expose;
             raise(SIGUSR1);
         }
-        XSendEvent(display,window,0,0,&temp);
+        XSendEvent(xwin.display,xwin.win,0,0,&temp);
 
         if(difference >= duration) timeup=true;
     }
     remove(OSD_VOLUME_LOCK);
-    
-    XDestroyWindow(display,window);
-    XCloseDisplay(display);
+}
+
+void draw(XWindow *xwin)
+{
+    volume = getvolume();
+    draw_vol(xwin);
+    draw_glyph(xwin);
+}
+
+void draw_vol(XWindow *xwin)
+{
+    XClearArea(xwin->display,
+               xwin->win,
+               xwin->width/2,
+               0,
+               0,
+               xwin->line1_h,
+               false);
+    if(is_muted()){
+        XftDrawStringUtf8(xwin->dc,
+                          &xwin->fgcolor,
+                          xwin->font,
+                          (xwin->font->max_advance_width*15)+xwin->margin,
+                          xwin->font->ascent,
+                          (FcChar8*)muted_str,
+                          5);  
+    }else{
+        uint32_t vol = (uint32_t)(getvolume()*100);
+        char volbuffer[3] = {' ',' ',' '};
+        for(int i=2; i>=0; --i){
+            volbuffer[i] = (vol%10)+'0';
+            if(vol<10) break;
+            vol /= 10;
+        }
+        
+        XftDrawStringUtf8(xwin->dc,
+                          &xwin->fgcolor,
+                          xwin->font,
+                          (xwin->font->max_advance_width*17)+xwin->margin,
+                          xwin->font->ascent,
+                          (FcChar8*)volbuffer,
+                          3);  
+        
+    }
+}
+
+void draw_glyph(XWindow *xwin)
+{
+    XClearArea(xwin->display,
+               xwin->win,
+               0,
+               xwin->line1_h,
+               0,
+               0,
+               false);
+    uint8_t nblock = (volume*100)/5;
+
+    XftDrawGlyphFontSpec(xwin->dc,
+                         &xwin->fgcolor,
+                         xwin->glyph,
+                         nblock);
+}
+
+void init_window(XWindow *xwin)
+{
+    uint8_t border_px=2;
+    XftColor color;
+    uint32_t screen_width=0;
+    uint32_t screen_height=0;
+
+    xwin->display  = XOpenDisplay(NULL);
+    xwin->screen   = DefaultScreen(xwin->display);
+    xwin->parent   = DefaultRootWindow(xwin->display);
+    xwin->depth    = DefaultDepth(xwin->display,xwin->screen);
+    xwin->cmap     = DefaultColormap(xwin->display,xwin->screen);
+    xwin->visual   = DefaultVisual(xwin->display,xwin->screen);
+    xwin->attrmask = CWOverrideRedirect|
+                          CWBorderPixel|
+                            CWBackPixel|
+                            CWEventMask;
+    xwin->xswa .override_redirect=true;
+    xwin->xswa.background_pixel=BLACK1;
+    xwin->xswa.border_pixel=WHITE1;
+    xwin->xswa.event_mask=ExposureMask| 
+                  VisibilityChangeMask| 
+                SubstructureNotifyMask;
+    screen_width   = XDisplayWidth(xwin->display,xwin->screen);
+    screen_height  = XDisplayHeight(xwin->display,xwin->screen);
+    xwin->font     = font_setup(xwin->display,xwin->screen,font_name2);
+    XftColorAllocName(xwin->display,xwin->visual,xwin->cmap,GREEN,&color);
+    xwin->fgcolor  = color;
+    xwin->margin   = 5;
+    xwin->width    = (xwin->font->max_advance_width*SZ)+
+                     (xwin->margin*2); 
+    xwin->height   = (xwin->font->ascent*2)+
+                     (xwin->font->descent)+
+                     (xwin->margin*2)+
+                     PX_BETWEEN;
+    xwin->xpos     = (screen_width*0.5)-(xwin->width*0.5);
+    xwin->ypos     = (screen_height*0.75);
+
+    xwin->win      = XCreateWindow(xwin->display, 
+                                   xwin->parent, 
+                                   xwin->xpos,
+                                   xwin->ypos, 
+                                   xwin->width,
+                                   xwin->height, 
+                                   border_px, 
+                                   xwin->depth, 
+                                   CopyFromParent, 
+                                   xwin->visual, 
+                                   xwin->attrmask, 
+                                   &xwin->xswa);
+
+    xwin->dc       = XftDrawCreate(xwin->display,
+                                   xwin->win,
+                                   xwin->visual,
+                                   xwin->cmap);
+
+    xwin->line1_h  = (xwin->font->ascent);
+    xwin->line2_h  = (xwin->line1_h*2)+xwin->margin;
+    xwin->glyph    = create_glyph(xwin,0x2588);
+
+    XMapWindow(xwin->display,xwin->win);
+    XSync(xwin->display,false);
+}
+
+void draw_label(XWindow *xwin, char *label)
+{
+    size_t length = strlen(label);
+    XftDrawStringUtf8(xwin->dc,
+                      &xwin->fgcolor,
+                      xwin->font, 
+                      xwin->margin, 
+                      xwin->line1_h, 
+                      (FcChar8*)label, 
+                      length);  
 }
 
 int is_muted(void)
@@ -205,19 +282,19 @@ int is_muted(void)
     return(ismuted);
 }
 
-XftGlyphFontSpec *create_spec(Display *display, XftFont *font, uint32_t glyph, uint32_t height, size_t sz)
+XftGlyphFontSpec *create_glyph(XWindow *xwin, uint32_t glyph)
 {
-    XftGlyphFontSpec *spec = malloc(sizeof(XftGlyphFontSpec)*sz);
+    XftGlyphFontSpec *spec = malloc(sizeof(XftGlyphFontSpec)*SZ);
 
-    int32_t xpos=padding, ypos=height;
-    for(size_t i=0; i<sz; ++i){
-        uint32_t idx = XftCharIndex(display,font,glyph);
+    int32_t xpos=xwin->margin, ypos=xwin->line2_h;
+    for(size_t i=0; i<SZ; ++i){
+        uint32_t idx = XftCharIndex(xwin->display,xwin->font,glyph);
         if(idx){
-            spec[i].font = font;
+            spec[i].font = xwin->font;
             spec[i].glyph = idx;
             spec[i].x = xpos;
             spec[i].y = ypos;
-            xpos += font->max_advance_width;
+            xpos += xwin->font->max_advance_width;
         }
     }
     return(spec);
